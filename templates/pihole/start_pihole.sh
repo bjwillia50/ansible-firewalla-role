8ca3c679b9ad9401bfb8fecf61f4a8d14e156ef1
Hector <calderon@hectors-mbp.lan>
2020-09-23 23:54:36 -0700

    first commit

---
+++ templates/pihole/start_pihole.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo ipset create -! docker_lan_routable_net_set hash:net
+sudo ipset add -! docker_lan_routable_net_set 172.16.0.0/24
+sudo ipset create -! docker_wan_routable_net_set hash:net
+sudo ipset add -! docker_wan_routable_net_set 172.16.0.0/24
+sudo systemctl start docker-compose@pi-hole
